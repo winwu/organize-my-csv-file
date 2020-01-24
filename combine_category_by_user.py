9feb64f795a5f324931cc81962fb2c0da0b44e43
winwu <look_me08@hotmail.com>
2020-01-24 22:57:56 +0800

    drop -2 value from by_alphabet csv

--- combine_category_by_user.py
+++ combine_category_by_user.py
@@ -134,7 +134,7 @@ def generate_summary_by_alphabet():
     for alphabet in english_category_range:
         alphabet_df = pd.DataFrame(index = list(range(1, total_test_user + 1)), columns = col_names)
         alphabet_df.index.name = 'tester'
-        alphabet_df.fillna(-1, inplace = True)
+        alphabet_df.fillna(-2, inplace = True)
         
         print('------------------------- alphabet: {} -----------------------------'.format(alphabet))
 
@@ -146,14 +146,20 @@ def generate_summary_by_alphabet():
             # pick sepecific row by alphabet
             row_data = user_summary_df.query('category == \'' + alphabet + '\'')
             
-            alphabet_df.at[i, 'Att_0s_avg'] = row_data.Att_0s_avg
-            alphabet_df.at[i, 'Att_1s_avg'] = row_data.Att_1s_avg
-            alphabet_df.at[i, 'Att_2s_avg'] = row_data.Att_2s_avg
-            alphabet_df.at[i, 'Med_0s_avg'] = row_data.Med_0s_avg
-            alphabet_df.at[i, 'Med_1s_avg'] = row_data.Med_1s_avg
-            alphabet_df.at[i, 'Med_2s_avg'] = row_data.Med_2s_avg
+            if ((row_data.Att_0s_avg != -1).bool() and (row_data.Att_1s_avg != -1).bool() and (row_data.Att_2s_avg != -1).bool() and (row_data.Med_0s_avg != -1).bool() and (row_data.Med_1s_avg != -1).bool() and (row_data.Med_2s_avg != -1).bool()):
+                alphabet_df.at[i, 'Att_0s_avg'] = row_data.Att_0s_avg
+                alphabet_df.at[i, 'Att_1s_avg'] = row_data.Att_1s_avg
+                alphabet_df.at[i, 'Att_2s_avg'] = row_data.Att_2s_avg
+                alphabet_df.at[i, 'Med_0s_avg'] = row_data.Med_0s_avg
+                alphabet_df.at[i, 'Med_1s_avg'] = row_data.Med_1s_avg
+                alphabet_df.at[i, 'Med_2s_avg'] = row_data.Med_2s_avg
         
+        # print pf without drop
         print(alphabet_df)
+        
+        # drop no-used row
+        alphabet_df.drop(alphabet_df.loc[alphabet_df['Att_0s_avg'] == -2].index, inplace = True)
+        
         alphabet_df.to_csv(dist_path + '/by_alphabet' + '/category_' +  alphabet + '.csv', encoding = 'utf-8', index = True)
 
 if __name__ == "__main__":
