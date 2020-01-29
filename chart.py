7233b384a477b98766d0098e16b06544e3470fd4
winwu <look_me08@hotmail.com>
2020-01-29 14:02:10 +0800

    generate all avg result of csv file

--- chart.py
+++ chart.py
@@ -19,20 +19,23 @@ from log_colors import log_colors
 # categories are named by English alphabet from A to I
 english_category_range = string.ascii_uppercase[:9]
 
+att_df_for_csv_list = list()
+med_df_for_csv_list = list()
+
 def initial():
     if not os.path.exists('./dist/images'):
         os.mkdir('./dist/images')
 
     if not os.path.exists('./dist/images/all_times_avg'):
         os.mkdir('./dist/images/all_times_avg')
-        
+
     if not os.path.exists('./dist/images/all_times_avg/attention'):
         os.mkdir('./dist/images/all_times_avg/attention')
     
     if not os.path.exists('./dist/images/all_times_avg/meditation'):
         os.mkdir('./dist/images/all_times_avg/meditation')
 
-def gen_all_avg_chart_by_category():
+def gen_all_avg_chart_by_category_of_each_user():
     for alphabet in english_category_range:
 
         the_csv = './dist/by_alphabet/category_' + alphabet + '.csv'
@@ -61,10 +64,18 @@ def gen_all_avg_chart_by_category():
         att_df.loc[1] = [1, a_1_avg]
         att_df.loc[2] = [2, a_2_avg]
 
+        att_df_for_csv = pd.DataFrame(columns = ['0s', '1s', '2s'])
+        att_df_for_csv.loc[alphabet] = [a_0_avg, a_1_avg, a_2_avg]
+        att_df_for_csv_list.append(att_df_for_csv)
+
         med_df = pd.DataFrame(columns = ['seconds', 'meditation_average'])
         med_df.loc[0] = [0, m_0_avg]
         med_df.loc[1] = [1, m_1_avg]
         med_df.loc[2] = [2, m_2_avg]
+
+        med_df_for_csv = pd.DataFrame(columns = ['0s', '1s', '2s'])
+        med_df_for_csv.loc[alphabet] = [m_0_avg, m_1_avg, m_2_avg]
+        med_df_for_csv_list.append(med_df_for_csv)
         
 
         # attention
@@ -72,15 +83,13 @@ def gen_all_avg_chart_by_category():
             '--------',
             'category: {} : the attention average of 3 times data of all users'.format(alphabet),
             att_df,
-             sep="\n")
-        
+            sep="\n")
         fig = px.line(att_df, x = 'seconds', y = 'attention_average', title = 'Average Result of all test of type: ' + alphabet)
         fig.update_layout(
             xaxis = dict(
                 dtick = 1
             )
         )
-
         fig.write_image('./dist/images/all_times_avg/attention/all_user_3_times_avg_category_' + alphabet + '.png', width = 700, scale = 2)
 
 
@@ -89,20 +98,41 @@ def gen_all_avg_chart_by_category():
             '--------',
             'category: {} : the meditation average of 3 times data of all users'.format(alphabet),
             med_df,
-             sep="\n")
-        
+            sep="\n")
         fig2 = px.line(med_df, x = 'seconds', y = 'meditation_average', title = 'Average Result of all test of type: ' + alphabet)
         fig2.update_layout(
             xaxis = dict(
                 dtick = 1
             )
         )
-
         fig2.write_image('./dist/images/all_times_avg/meditation/all_user_3_times_avg_category_' + alphabet + '.png', width = 700, scale = 2)
             
         #fig.show()
 
+def gen_all_user_of_3_times_avg_csv():
+    
+    print('att_df_for_csv_list')
+    att_avg_df = pd.concat(att_df_for_csv_list)
+    att_avg_df.index.name = 'category'
+    att_avg_df.to_csv('./dist/images/all_times_avg/attention/all.csv', encoding = 'utf-8', index = True)
+    print(att_avg_df)
+    
+    
+    print('med_df_for_csv_list')
+    med_avg_df = pd.concat(med_df_for_csv_list)
+    med_avg_df.index.name = 'category'
+    med_avg_df.to_csv('./dist/images/all_times_avg/meditation/all.csv', encoding = 'utf-8', index = True)
+    print(med_avg_df)
+
+
 if __name__ == "__main__":
-   # total_test_user = int(get_total_user_val(sys.argv[1:]))
-   initial()
-   gen_all_avg_chart_by_category()
+    # total_test_user = int(get_total_user_val(sys.argv[1:]))
+    initial()
+    
+    # 產生每個類別所有使用者三次三秒平均數值
+    gen_all_avg_chart_by_category_of_each_user()
+    
+    # 產生所有類別三次平均數值的 csv
+    gen_all_user_of_3_times_avg_csv()
+    
+    
